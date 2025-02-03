4255d93335506d63fbb7235a2a5a1158a8c79ed1
Poonam606 <poonamyadav464@gmail.com>
2025-02-03 21:06:55 +0530

    feeditem added

---
+++ EssentialFeed/Feeditem.swift
@@ -0,0 +1,14 @@
+//
+//  Feeditem.swift
+//  EssentialFeed
+//
+//  Created by poonam on 03/02/25.
+//
+
+import Foundation
+struct Feeditem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
